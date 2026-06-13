6d8757494a5298d9434174a2e661a87e7ca2b64a
Sunay16018 <seyidlisunay20@gmail.com>
2026-06-13 13:13:55 +0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
-  throw new Error('JWT_SECRET environment variable is required');
+  console.error('JWT_SECRET environment variable is required');
+  process.exit(1);
 }
 
 async function authenticate(req, res, next) {
