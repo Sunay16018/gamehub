105476755dc5ea837e2209fb4e1a7cc12738f79b
Sunay16018 <seyidlisunay20@gmail.com>
2026-06-13 13:13:27 +0400

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,7 +3,8 @@ const { MongoClient } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
-  throw new Error('MONGODB_URI environment variable is required');
+  console.error('MONGODB_URI environment variable is required');
+  process.exit(1);
 }
 
 const client = new MongoClient(uri, {
